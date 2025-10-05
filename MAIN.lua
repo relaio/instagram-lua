64afc437408fc60d6d0989300b131a5c55ad5031
Kartik <kartik@192.168.1.4>
2025-10-05 16:32:56 +0530

    startup test

---
+++ MAIN.lua
@@ -0,0 +1,10 @@
+require 'instagram'
+require 'utils'
+require 'network'
+
+DEVICE_ID = UTILS.fetch_device_id()
+UTILS.log_message('Device ID: ' .. DEVICE_ID)
+
+UTILS.startup()
+
+INSTAGRAM.new_instagram()
--- instagram.lua
+++ instagram.lua
@@ -5,8 +5,51 @@ INSTAGRAM = {
 }
 
 
-function INSTAGRAM.new_instagram() 
+function INSTAGRAM.new_instagram(prefix) 
+    local container_name = prefix and prefix .. '-' .. tostring(os.time()) or tostring(os.time())
+    local container_uuid = UTILS.create_app_container(INSTAGRAM.bid, container_name)
 
+    local to_return = false
+    local attempts = 1
+
+    while not container_uuid do
+        if attempts > 3 then
+            to_return = true
+            break
+        end
+        sys.sleep(attempts)
+        container_name = tostring(os.time())
+        container_uuid = UTILS.create_app_container(INSTAGRAM.bid, container_name)
+        attempts = attempts + 1
+    end
+    if to_return then
+        UTILS.log_message('Could not create new Instagram container after 3 retries.')
+        return nil, nil, {
+            message = 'Could not create new Instagram container after 3 retries.'
+        }
+    end
+
+    UTILS.log_message('Instagram container ' .. container_name .. ' created.')
+
+    local opened = UTILS.open_app_container(INSTAGRAM.bid, container_uuid)
+    attempts = 1
+
+    while not opened do
+        if attempts > 3 then
+            to_return = true
+            break
+        end
+        sys.sleep(attempts)
+        opened = UTILS.open_app_container(INSTAGRAM.bid, container_uuid)
+        attempts = attempts + 1
+    end
+    if to_return then
+        return nil, nil, {
+            message = 'Failed to open Instagram container after 3 retries.'
+        }
+    end
+
+    return container_name, container_uuid
 end
 
 
