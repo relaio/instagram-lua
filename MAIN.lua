aebfd75a9500b9f4f2e587c0a029f6847dc07bb7
Kartik <kartik@192.168.1.4>
2025-10-06 21:40:28 +0530

    idk

--- MAIN.lua
+++ MAIN.lua
@@ -2,9 +2,29 @@ require 'instagram'
 require 'utils'
 require 'network'
 
+
+-- STARTUP
+
 DEVICE_ID = UTILS.fetch_device_id()
 UTILS.log_message('Device ID: ' .. DEVICE_ID)
 
 UTILS.startup()
 
-INSTAGRAM.new_instagram('dev')
+
+-- GLOBALS
+
+CONTAINER_NAME = nil
+CONTAINER_UUID = nil
+
+
+-- MAIN
+
+do -- New Instagram
+    local container_name, container_uuid, error_data = INSTAGRAM.new_instagram('dev')
+    if not container_name and not container_uuid then
+        UTILS.log_message('[CRITICAL] Failed to create and open new Instagram container. Error message: ' .. (error_data and error_data.message or 'nil'))
+        os.exit(1)
+    end
+    CONTAINER_NAME = container_name
+    CONTAINER_UUID = container_uuid
+end
