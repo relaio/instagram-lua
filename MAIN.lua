a2f6877627c813824c6da82391d42073bb4a7d4c
Kartik <kartik@192.168.1.3>
2025-10-18 12:16:55 +0530

    delete all containers

--- MAIN.lua
+++ MAIN.lua
@@ -19,19 +19,31 @@ CONTAINER_UUID = nil
 
 -- MAIN
 
-do -- New Instagram
-    local container_data, error_data = INSTAGRAM.new_instagram('dev')
-    if not container_data then
-        UTILS.log_message('[CRITICAL] Failed to create and open new Instagram container. Error message: ' .. (error_data and error_data.message or 'nil'))
-        os.exit(1)
-    end
-    CONTAINER_NAME = container_data.container_name
-    CONTAINER_UUID = container_data.container_uuid
-end
+-- do -- New Instagram
+--     local container_data, error_data = INSTAGRAM.new_instagram('dev')
+--     if not container_data then
+--         UTILS.log_message('[CRITICAL] Failed to create and open new Instagram container. Error message: ' .. (error_data and error_data.message or 'nil'))
+--         os.exit(1)
+--     end
+--     CONTAINER_NAME = container_data.container_name
+--     CONTAINER_UUID = container_data.container_uuid
+-- end
 
-
-UTILS.log_message(CONTAINER_NAME)
-
-sys.sleep(3)
-
-UTILS.delete_container(CONTAINER_NAME)
+UTILS.delete_container('js')
+UTILS.delete_container('jsjd')
+UTILS.delete_container('gahsn')
+UTILS.delete_container('kaosh')
+UTILS.delete_container('aaaa')
+UTILS.delete_container('bhhb')
+UTILS.delete_container('babbbx')
+UTILS.delete_container('abbbd')
+UTILS.delete_container('agbsbx')
+UTILS.delete_container('absnnx')
+UTILS.delete_container('avbxb')
+UTILS.delete_container('ahsbbxh')
+UTILS.delete_container('abbxjx')
+UTILS.delete_container('sbxbbc')
+UTILS.delete_container('bshsns')
+UTILS.delete_container('chansndn')
+UTILS.delete_container('jansbxhx')
+UTILS.delete_container('avbbb')
